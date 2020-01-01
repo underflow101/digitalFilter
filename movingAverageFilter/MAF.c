81e020fd294b73e49fe5c0fa4557b6396f13985b
Dongwon Paek <ikarus125@gmail.com>
2020-01-01 21:56:19 +0900

    Update MAF.c

--- movingAverageFilter/MAF.c
+++ movingAverageFilter/MAF.c
@@ -1,3 +1,32 @@
 // Moving Average Filter
 // Decrease noise with dynamic range of input signal
 
+#define FILTERSIZE 10
+
+int flag = 0;
+
+int data[FILTERSIZE];   // Make this as global variable
+
+void MAF(int *_input) {
+    int sum, avg;
+    
+    for(int i = 0; i < FILTERSIZE; i++) {
+        sum += data[i];
+        sum += *_input;
+    }
+    avg = sum / (FILTERSIZE + 1);
+    *_input = avg;
+}
+
+void _collectData(int _data) {
+    if(flag < FILTERSIZE) {
+        data[flag] = _data;
+        flag++;
+    } else {
+        for(int i = 0; i < FILTERSIZE; i++) {
+            data[i] = data[i+1];
+        }
+        MAF(*_data);
+        data[FILTERSIZE-1] = _data;
+    }
+}
